fd017304646cad84303c55574f075f14a2b236b3
sulaimanbehzad <sulaiman.behzad@gmail.com>
2020-07-09 12:48:58 +0430

    students index edited table added

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -14,15 +14,42 @@
         @endphp
         <br><br><br>
         @foreach($courses_grouped as $group => $terms)
-        {{"گروه ".$group}} <br>
+        <h4>{{"گروه ".$group}} </h4>
         @foreach($terms as $term => $courses)
-        {{"term".$term}} <br>
-        @foreach($courses as $course)
-        <li>
-            {{$course}}
-            <a href="{{ route('courses.addToCart',['id' => $course->id]) }}">اخذ</a>
-        </li>
-        @endforeach
+        <h5>{{"term".$term}} </h5>
+        <table class="table table-sm" id="courses table">
+            <thead>
+                <tr>
+                    <th>شماره درس</th>
+                    <th>سال تحصیلی</th>
+                    <th>ترم تحصیلی</th>
+                    <th>نام درس</th>
+                    <th>تعداد واحد</th>
+                    <th>تعداد دانشجو</th>
+                    <th>شماره استاد</th>
+                    <th>شماره گروه</th>
+                    <!-- <th></th> -->
+                </tr>
+            </thead>
+            <tbody>>
+                <tr>
+                    @foreach($courses as $course)
+                    <td>{{$course->id}}</td>
+                    <td>{{$course->year}}</td>
+                    <td>{{$course->term}}</td>
+                    <td>{{$course->name}}</td>
+                    <td>{{$course->unit_no}}</td>
+                    <td>{{$course->student_no}}</td>
+                    <td>{{$course->teacher_id}}</td>
+                    <td>{{$course->group_id}}</td>
+                    <td>
+                        <!-- {{$course}} -->
+                        <button class="btn btn-primary"><a class="text-light" href="{{ route('courses.addToCart',['id' => $course->id]) }}">اخذ</a></button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
         <br>
         @endforeach
         <br><br><br><br>
