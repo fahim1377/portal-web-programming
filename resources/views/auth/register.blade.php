629e56b170c4d71dbd7beb07ded68f5453f33cbd
sulaimanbehzad <sulaiman.behzad@gmail.com>
2020-07-15 20:26:11 +0430

    register for teacher and student added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,10 +2,19 @@
 
 @section('content')
 <div class="container auth-page">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div id="headerAuthPage" class="card col-sm-12" style="padding: 20px;">
+        <h3>ثبت نام</h3>
+        <select id="selectForm" class="browser-default custom-select col-md-2">
+            <option selected>نوع کاربر</option>
+            <option value="1">دانشجو</option>
+            <option value="2">استاد</option>
+        </select>
+    </div>
+
+    <div id="studentRegisterForm" class="row justify-content-md-center">
+        <div class="col-sm-12">
             <div class="card">
-                <div class="card-header">{{ __('ثبت نام') }}</div>
+                <div class="card-header">{{ __('ثبت نام‌ دانشجو') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -17,9 +26,9 @@
                             <div class="col-md-6">
                                 <input id="fname" type="text" class="form-control @error('fname') is-invalid @enderror" name="fname" value="{{ old('name') }}" required autocomplete="fname" autofocus>
                                 @error('fname')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -31,8 +40,8 @@
                                 <input id="lname" type="text" class="form-control @error('lname') is-invalid @enderror" name="lname" value="{{ old('lname') }}" required autocomplete="lname" autofocus>
                                 @error('lname')
                                 <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -44,8 +53,8 @@
                                 <input id="id" type="text" class="form-control @error('id') is-invalid @enderror" name="id" value="{{ old('id') }}" required autocomplete="id" autofocus>
                                 @error('id')
                                 <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -57,13 +66,13 @@
                             <div class="col-md-6">
                                 <select id="group_id" name="group_id">
                                     @foreach($groups as $group)
-                                        <option value={{$group->id}}>{{$group->name}}</option>
+                                    <option value={{$group->id}}>{{$group->name}}</option>
                                     @endforeach
                                 </select>
                                 @error('group_id')
                                 <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -83,9 +92,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -99,18 +108,115 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                {{--<button type="submit" class="btn btn-primary">--}}
-                                    {{--{{ __('Register') }}--}}
-                                {{--</button>--}}
-                                {{--recaptcha--}}
-                                <button
-                                        class="g-recaptcha"
-                                        data-sitekey="6Lfaxa8ZAAAAAB5QOeTvl3Wwd3DGRvLG0IIEG65Z"
-                                        data-callback="onSubmit">
-                                    Submit
+                            <div class="col-md-6">
+                                <button class="btn btn-primary" type="submit">
+                                    ثبت نام
                                 </button>
-                                {{--endpart--}}
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="teacherRegisterForm" class="row justify-content-md-center">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">{{ __('ثبت نام استاد') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="first name" class="col-md-4 col-form-label text-md-right">{{ __('نام') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="fname" type="text" class="form-control @error('fname') is-invalid @enderror" name="fname" value="{{ old('name') }}" required autocomplete="fname" autofocus>
+                                @error('fname')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="last name" class="col-md-4 col-form-label text-md-right">{{ __('نام خانوادگی') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="lname" type="text" class="form-control @error('lname') is-invalid @enderror" name="lname" value="{{ old('lname') }}" required autocomplete="lname" autofocus>
+                                @error('lname')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="id" class="col-md-4 col-form-label text-md-right">{{ __('شماره پرسنلی') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="id" type="text" class="form-control @error('id') is-invalid @enderror" name="id" value="{{ old('id') }}" required autocomplete="id" autofocus>
+                                @error('id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="academicRank" class="col-md-4 col-form-label text-md-right">{{ __('ٰمرتبه علمی') }}</label>
+
+
+                            <div class="col-md-6">
+                                <input id="academicRank" type="text" class="form-control @error('academic_rank') is-invalid @enderror" autofocus>
+                                @error('academicRank')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('آدرس ایمیل') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('رمز عبور') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('تاییدی رمز عبور') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6">
+                                <button class="btn btn-primary" type="submit">
+                                    ثبت نام
+                                </button>
                             </div>
                         </div>
                     </form>
@@ -119,4 +225,20 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $('#selectForm').on('change', function() {
+            if (this.value == '1')
+            //.....................^.......
+            {
+                $("#studentRegisterForm").show();
+                $("#teacherRegisterForm").hide();
+            } else if (this.value == '2') {
+                $("#studentRegisterForm").hide();
+                $("#teacherRegisterForm").show();
+            }
+        });
+    });
+</script>
+<!-- hidden  name="s0Rp" key="student" -->
 @endsection('content')
